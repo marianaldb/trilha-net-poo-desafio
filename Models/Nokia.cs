abcea7b838741a37f32b4a01eea933c11c408dd8
marianaldb <154998976+marianaldb@users.noreply.github.com>
2024-01-13 00:09:44 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,26 +1,36 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia : Smartphone
+    public abstract class Smartphone
     {
-        public Nokia(string numero, string imei,string marca, string modelo, int memoria, int armazenamento) : base(numero, imei,marca, modelo, memoria, armazenamento)
-        {}
-        // TODO: Sobrescrever o método "InstalarAplicativo"
-        public override void InstalarAplicativo(string nomeApp)
+         public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string IMEI { get; set; }
+        protected string Marca { get; set; }
+        protected string Modelo { get; set; }
+        protected int Memoria { get; set; }
+        protected int Armazenamento { get; set; }
+
+        public Smartphone(string numero, string imei,string marca, string modelo, int memoria, int armazenamento)
         {
-            if(string.IsNullOrWhiteSpace(nomeApp))
-                throw new ArgumentNullException(nomeApp, "O nome do aplicativo deve ser informado.");
-            else
-            {
-                Console.WriteLine($"Instalando {nomeApp}...");
-                for (int i = 1; i <= 3; i++)
-                {
-                    Console.WriteLine($"{i}...");
-                    Thread.Sleep(1000);
-                }
-                Console.WriteLine($"{nomeApp} instalado com sucesso!");
-            }
-            
+            Numero = numero;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+            IMEI = imei;
+            Marca = marca;
+            Modelo = modelo;
+            Memoria = memoria;
+            Armazenamento = armazenamento;
         }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
     }
 }
