ed598a4cab98ba9e3d2f2b40d86af2c72d755e95
marianaldb <154998976+marianaldb@users.noreply.github.com>
2024-01-13 00:07:02 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,26 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+   // TODO: Herdar da classe "Smartphone"
+    public class Apple : Smartphone
     {
+        public Apple(string numero, string imei,string marca, string modelo, int memoria, int armazenamento) : base(numero, imei,marca, modelo, memoria, armazenamento)
+        {}
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            if(string.IsNullOrWhiteSpace(nomeApp))
+                throw new ArgumentNullException("O nome do aplicativo deve ser informado.");
+            else
+            {
+                Console.WriteLine($"Instalando {nomeApp}...");
+                for (int i = 1; i <= 3; i++)
+                {
+                    Console.WriteLine($"{i}...");
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine($"{nomeApp} instalado com sucesso!");
+            }
+            
+        }
     }
 }
