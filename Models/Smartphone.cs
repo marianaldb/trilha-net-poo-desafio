1461bf8838419056009448da0751fcf343800a30
marianaldb <154998976+marianaldb@users.noreply.github.com>
2024-01-13 00:12:28 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,23 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
+         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string IMEI { get; set; }
+        protected string Marca { get; set; }
+        protected string Modelo { get; set; }
+        protected int Memoria { get; set; }
+        protected int Armazenamento { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string imei,string marca, string modelo, int memoria, int armazenamento)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            IMEI = imei;
+            Marca = marca;
+            Modelo = modelo;
+            Memoria = memoria;
+            Armazenamento = armazenamento;
         }
 
         public void Ligar()
